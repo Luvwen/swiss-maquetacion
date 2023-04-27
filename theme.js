7233dc8707b0946aaab8e8f8c5731d52c885018c
Santiago Baliño <santiagobalino@hotmail.com>
2023-04-27 20:22:06 -0300

    Add theme for chakra ui

--- theme.js
+++ theme.js
@@ -4,6 +4,11 @@ const theme = extendTheme({
     fonts: {
         heading: `'Source Sans Pro', sans-serif`, //eslint-disable-line
         body: `'Source Sans Pro', sans-serif` //eslint-disable-line
+    },
+    colors: {
+        primary: '#606060',
+        secondary: '#ffffff',
+        hover: '#21232a'
     }
 });
 
